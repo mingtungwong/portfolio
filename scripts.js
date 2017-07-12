6e7a32d3febe92f60915f60398966dfdfc0c26ad
Ming Tung Wong <mingtungwong@outlook.com>
2017-07-12 18:40:55 -0400

    Add startup script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+function setup() {
+    document.getElementById("menu_home").classList.add("active_menu");
+    let pages = document.getElementsByClassName("page");
+    for(let page of pages){
+        page.style.display = "none";
+    }
+    document.getElementById("home").style.display = "block";
+}
+
 function changeActive(element) {
     let list = element.parentElement.getElementsByTagName("li");
     for(let li of list) {
