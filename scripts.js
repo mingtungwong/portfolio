13b057d12eebf35827478f6d5a13d7b4bfa6d5c8
Ming Tung Wong <mingtungwong@outlook.com>
2017-07-12 18:31:43 -0400

    Modify some css, add skills page, add code to hide and show various pages

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function changeActive(element) {
     let list = element.parentElement.getElementsByTagName("li");
-    for(li of list) {
+    for(let li of list) {
         li.classList.remove("active_menu");
     }
     element.classList.add("active_menu");
+
+    let div = element.id.split("_")[1];
+    let pages = document.getElementsByClassName("page");
+    for(let page of pages) {
+        page.style.display = "none";
+    }
+    document.getElementById(div).style.display = "block";
 }
