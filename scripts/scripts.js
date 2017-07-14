23cb88a672b12a320e78872b6960cf2e6833aff7
Ming Tung Wong <mingtungwong@outlook.com>
2017-07-14 17:04:00 -0400

    removed setup script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,3 @@
-function setup() {
-    document.getElementById("menu_home").classList.add("active_menu");
-    let pages = document.getElementsByClassName("page");
-    for(let page of pages){
-        page.style.display = "none";
-    }
-    document.getElementById("home").style.display = "block";
-}
-
 function changeActive(element) {
     let list = element.parentElement.getElementsByTagName("li");
     for(let li of list) {
